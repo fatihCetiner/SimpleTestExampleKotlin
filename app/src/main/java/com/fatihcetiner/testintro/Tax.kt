3d8b7f54e9ef90d074e504e33c9f16ea5107e4ab
fatihCetiner <f-cetiner99@hotmail.com>
2023-01-11 17:54:32 +0300

    Simple Test Example Kotlin

---
+++ app/src/main/java/com/fatihcetiner/testintro/Tax.kt
@@ -0,0 +1,16 @@
+package com.fatihcetiner.testintro
+
+class Tax {
+
+    // TDD -> Test Driven Development
+
+    fun calculateTax(grossIncome: Double, taxRate: Double): Double{
+        return grossIncome * taxRate
+    }
+
+    fun calculateIncome(grossIncome: Double, taxRate: Double): Double{
+        return grossIncome - (grossIncome * taxRate)
+    }
+
+
+}
---
+++ app/src/test/java/com/fatihcetiner/testintro/TestTax.kt
@@ -0,0 +1,33 @@
+package com.fatihcetiner.testintro
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class TestTax {
+
+    private lateinit var tax: Tax
+
+    @Before
+    fun setup(){
+        tax = Tax()
+    }
+
+    @After
+    fun teardown(){
+
+    }
+
+    @Test
+    fun calculateTaxText(){
+        val netTax = tax.calculateTax(100.0,0.1)
+        assertThat(netTax).isEqualTo(10)
+    }
+
+    @Test
+    fun calculateIncomeTest() {
+        val netIncome = tax.calculateIncome(100.0,0.1)
+        assertThat(netIncome).isEqualTo(90)
+    }
+}
